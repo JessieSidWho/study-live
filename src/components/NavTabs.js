6f4f114f785f3ce3119d325f56fb7b2405a990f8
Jessie Sidhu <jsidhu22@gmail.com>
2019-04-22 14:06:04 -0700

    fixed navbar a little

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,44 +2,42 @@ import React from "react";
 
 function NavTabs(props) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => props.handlePageChange("Home")}
-          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Home
+    
+      <ul className="nav nav-tabs row">
+        <li className="nav-item col-md-2 pt-2 ml-2">
+          
+            Jessie Sidhu
+        
+        </li>
+        <li className="nav-item col-md-2 offset-1">
+          <a
+            href="#home"
+            onClick={() => props.handlePageChange("Home")}
+            className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+          >
+            Home
         </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => props.handlePageChange("About")}
-          className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
-        >
-          About
+        </li>
+        <li className="nav-item col-md-2">
+          <a
+            href="#porfolio"
+            onClick={() => props.handlePageChange("Portfolio")}
+            className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
+          >
+            Portfolio
         </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => props.handlePageChange("Blog")}
-          className={props.currentPage === "Blog" ? "nav-link active" : "nav-link"}
-        >
-          Blog
+        </li>
+        <li className="nav-item col-md-2">
+          <a
+            href="#contact"
+            onClick={() => props.handlePageChange("Contact")}
+            className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+          >
+            Contact
         </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => props.handlePageChange("Contact")}
-          className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+        </li>
+      </ul>
+    
   );
 }
 
