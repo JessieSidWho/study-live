23c28b35710d078ee07654f477c3c0e79fc187f8
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-03 15:01:13 -0700

    light-theme added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form'
 import authReducer from './authReducer';
+import colorReducer from './colorReducer';
 
 
 export default combineReducers({
     
     auth: authReducer,
     form: reduxForm,
+    color: colorReducer,
     
 });
