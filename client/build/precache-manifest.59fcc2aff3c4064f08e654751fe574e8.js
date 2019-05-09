e8fc3110f6c8c082c10137e54850163efa8983dc
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-09 11:14:33 -0700

    newnew

---
+++ client/build/precache-manifest.59fcc2aff3c4064f08e654751fe574e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26c88551233e85a5528f",
+    "url": "/static/css/main.f60913c8.chunk.css"
+  },
+  {
+    "revision": "26c88551233e85a5528f",
+    "url": "/static/js/main.412e4b49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb10409a778aa253ddb6",
+    "url": "/static/js/2.70ed2a9b.chunk.js"
+  },
+  {
+    "revision": "3f6ed800f6b809824ebd6584abef32c7",
+    "url": "/index.html"
+  }
+];
