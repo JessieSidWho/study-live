c21c9b7d22feebd608cac68b5fd073cc235ca5a1
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-12 16:58:40 -0700

    heroku branch for deployment fix

---
+++ client/build/precache-manifest.c066aec968a03ee750dff4776002c3f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a41dc419c1621b4f99e8",
+    "url": "/static/css/main.f60913c8.chunk.css"
+  },
+  {
+    "revision": "a41dc419c1621b4f99e8",
+    "url": "/static/js/main.5b6f4263.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "928c8919de2c26c4c028",
+    "url": "/static/js/2.34a0ebfb.chunk.js"
+  },
+  {
+    "revision": "95840bedabf31cd6aef60c8f067f958c",
+    "url": "/index.html"
+  }
+];
