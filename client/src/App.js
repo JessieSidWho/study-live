cde07f5ac52bed8cd43fd5950155ef060772950a
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-14 15:09:10 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Login from './components/Login';
 import Main from './components/Main';
 import './index.css';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from 'react-redux';
 import { fetchUser } from './actions';
 import { colorChange } from './actions';
@@ -32,12 +32,12 @@ class App extends Component {
                 <div className={`container-fluid h-100 ${this.props.color}`}>
                     <Route exact path='/' component={Login} />
                     <Route exact path='/main' component={Main} />
-                    {/* <Route exact path='/lofi' component={Main} />
+                    <Route exact path='/lofi' component={Main} />
                     <Route exact path='/reggae' component={Main} />
                     <Route exact path='/indie' component={Main} />
                     <Route exact path='/jazz' component={Main} />
                     <Route exact path='/morningcoffee' component={Main} />
-                    <Route exact path='/metal' component={Main} /> */}
+                    <Route exact path='/metal' component={Main} />
                 </div>
             </Router>
             
