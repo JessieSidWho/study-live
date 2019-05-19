df911952bbf9db5c8872d207e33a81410ed4275f
Odysseus <arwaterman@gmail.com>
2019-05-19 14:54:16 -0700

    attempted fix for chat v2

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("./config/connection");
 const express = require('express');
 const app = express();
 
+
 // Routes to Mongo DB
 const chatRouter = require('./routes/chat');
 
@@ -13,9 +14,16 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
+// Add routes, both API and view
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 // socket io
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+// const server = require('http').createServer(app);
+const io = require('socket.io')(app);
 
 // google
 const routes = require("./routes");
@@ -171,8 +179,8 @@ app.get('/users', async (req, res) => {
 })
 
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
