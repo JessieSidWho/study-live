69dacfce80bfe1709f6314924fe0393b5d2f92f3
Odysseus <arwaterman@gmail.com>
2019-05-23 13:54:13 -0700

    updating index routing file

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,6 @@ const apiRoutes = require("./api");
 const chatRouter = require("./chat");
 const db = require('./../model');
 
-router.get('/users', async (req, res) => {
-    console.log(req.user);
-    
-    const users = await db.User.find();
-    res.json(users);
-});
-
-// Handles saving the Chat messages to Mongo DB
-router.use('/chat', chatRouter);
-
-// API Routes
-router.use("/api", apiRoutes);
-
 // If no API routes are hit, send the React app
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/public"));
@@ -30,5 +17,18 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "/client/public/index.html"));
   });
 }
+
+router.get('/users', async (req, res) => {
+    console.log(req.user);
+    
+    const users = await db.User.find();
+    res.json(users);
+});
+
+// Handles saving the Chat messages to Mongo DB
+router.use('/chat', chatRouter);
+
+// API Routes
+router.use("/api", apiRoutes);
 
 module.exports = router;
