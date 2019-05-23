957de355ab2b6357e7d8a129f897c5ced2461e84
Odysseus <arwaterman@gmail.com>
2019-05-23 14:01:56 -0700

    updating route index file

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,16 @@ const apiRoutes = require("./api");
 const chatRouter = require("./chat");
 const db = require('./../model');
 
-// If no API routes are hit, send the React router
+// If no API routes are hit, send to the React app
 if (process.env.NODE_ENV === "production") {
-  router.use(express.static("client/public"));
+  router.use(express.static("client/build"));
   router.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 } else {
   router.use(express.static(path.join(__dirname, '/client/public')));
   router.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
   });
 }
 
