d46e74c642745ca7eb434797edfb83357ba856c1
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-25 01:15:51 -0700

    test

--- job.js
+++ job.js
@@ -1,7 +1,12 @@
 // const moment = require('moment');
 const moment = require('moment-timezone');
+const ping = require('node-http-ping')
 
 function sayHello() {
-    console.log('Heroku Schehuler pinged the Study-Live site at: ', moment().tz("America/Los_Angeles").format('MMMM Do YYYY, h:mm:ss a'));
+    ping('https://study-live.herokuapp.com')
+    .then(time => console.log(`Response time: ${time}ms`))
+    .catch(() => console.log('Failed to ping study-live.herokuapp.com'))
+
+    console.log('Heroku Schehuler ran the ping at: ', moment().tz("America/Los_Angeles").format('MMMM Do YYYY, h:mm:ss a'));
 }
 sayHello();
