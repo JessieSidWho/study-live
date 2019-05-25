9d1d053a89a1e8591507a761e835b62378fb268b
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-24 23:48:23 -0700

    removed study live chat comment

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -116,7 +116,7 @@ class Chat extends Component {
 
                 <div id="chat" className="col-sm-12 pt-3" >
                     <p>
-                        Study Live: Hello, Our Chat feature is disabled at the moment. We apologize for the inconvenience.
+                        
                     </p>
                 </div>
 
