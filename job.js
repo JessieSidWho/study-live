28758f219b4005c08e4f3347197b4cc193de8c24
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-25 01:04:43 -0700

    test

--- job.js
+++ job.js
@@ -1,6 +1,7 @@
-const moment = require('moment');
+// const moment = require('moment');
+const moment = require('moment-timezone');
 
 function sayHello() {
-    console.log('Heroku Schehuler pinged the Study-Live site at: ', moment().format('MMMM Do YYYY, h:mm:ss a'));
+    console.log('Heroku Schehuler pinged the Study-Live site at: ', moment().tz("America/Los_Angeles").format('MMMM Do YYYY, h:mm:ss a'));
 }
 sayHello();
